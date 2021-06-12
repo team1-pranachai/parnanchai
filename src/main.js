88210a7cf0cf018d0a014a3da8747d82dd9fcb86
VTR-xyh <253676328@qq.com>
2021-06-12 09:25:33 +0800

    packjson

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'jquery'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
+
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 new Vue({
